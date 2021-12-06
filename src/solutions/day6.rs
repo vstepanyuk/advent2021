b6ce02471f5588f064f557f364a7fd3e5749e533
Vitali Stsepaniuk <contact@vitaliy.dev>
2021-12-06 07:05:59 +0100

    refactor: day6

--- src/solutions/day6.rs
+++ src/solutions/day6.rs
@@ -1,38 +1,12 @@
 use crate::solutions::{Result, Solution};
-use std::collections::HashMap;
 use std::fmt::Display;
 
 #[derive(Default)]
 pub struct DaySolution;
 
-impl Solution for DaySolution {
-    fn new() -> Self {
-        Self {}
-    }
-
-    fn part_1(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
-        let mut fish: Vec<i32> = input
-            .unwrap()
-            .split(',')
-            .filter_map(|s| s.parse::<i32>().ok())
-            .collect();
-
-        for i in 0..80 {
-            for j in 0..fish.len() {
-                if fish[j] == 0 {
-                    fish[j] = 6;
-                    fish.push(8);
-                } else {
-                    fish[j] -= 1;
-                }
-            }
-        }
-
-        Ok(Box::new(fish.len()))
-    }
-
-    fn part_2(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
-        let mut fish: Vec<i32> = input
+impl DaySolution {
+    fn solve(&self, input: Option<String>, days: usize) -> u64 {
+        let fish: Vec<i32> = input
             .unwrap()
             .split(',')
             .filter_map(|s| s.parse::<i32>().ok())
@@ -44,7 +18,7 @@ impl Solution for DaySolution {
             days_fish[f as usize] += 1;
         });
 
-        for _ in 0..256 {
+        for _ in 0..days {
             let count = days_fish[0];
             for i in 0..8 {
                 days_fish[i] = days_fish[i + 1];
@@ -54,7 +28,21 @@ impl Solution for DaySolution {
             days_fish[8] = count;
         }
 
-        Ok(Box::new(days_fish.iter().sum::<u64>()))
+        days_fish.iter().sum::<u64>()
+    }
+}
+
+impl Solution for DaySolution {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn part_1(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
+        Ok(Box::new(self.solve(input, 80)))
+    }
+
+    fn part_2(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
+        Ok(Box::new(self.solve(input, 256)))
     }
 }
 
