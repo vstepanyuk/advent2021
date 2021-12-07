ce27b1df4febac2dc7b8ffd4cb4b923076d5e200
Vitali Stsepaniuk <contact@vitaliy.dev>
2021-12-07 06:46:34 +0100

    refactor: day7

--- src/solutions/day7.rs
+++ src/solutions/day7.rs
@@ -1,9 +1,30 @@
-use crate::solutions::{Result, Solution};
-use std::cmp::min;
 use std::fmt::Display;
+
+use crate::solutions::{Result, Solution};
 
 #[derive(Default)]
 pub struct DaySolution;
+
+impl DaySolution {
+    fn solve<F>(&self, input: Option<String>, cost: F) -> i32
+    where
+        F: Fn(i32, i32) -> i32,
+    {
+        let data: Vec<i32> = input
+            .unwrap()
+            .split(',')
+            .filter_map(|s| s.parse::<i32>().ok())
+            .collect();
+
+        let min_align = *data.iter().min().unwrap();
+        let max_align = *data.iter().max().unwrap();
+
+        (min_align..max_align)
+            .map(|align| data.iter().map(|&v| cost(v, align)).sum())
+            .min()
+            .unwrap()
+    }
+}
 
 impl Solution for DaySolution {
     fn new() -> Self {
@@ -11,51 +32,13 @@ impl Solution for DaySolution {
     }
 
     fn part_1(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
-        let data: Vec<i32> = input
-            .unwrap()
-            .split(',')
-            .filter_map(|s| s.parse::<i32>().ok())
-            .collect();
-
-        let mut min_sum = i32::MAX;
-        let min_fuel = *data.iter().min().unwrap();
-        let max_fuel = *data.iter().max().unwrap();
-
-        for i in min_fuel..max_fuel {
-            let sum: i32 = data.iter().map(|v| (v - i).abs()).sum();
-            min_sum = min(sum, min_sum);
-        }
-
-        Ok(Box::new(min_sum))
+        Ok(Box::new(self.solve(input, |v, a| (v - a).abs())))
     }
 
     fn part_2(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
-        let data: Vec<i32> = input
-            .unwrap()
-            .split(',')
-            .filter_map(|s| s.parse::<i32>().ok())
-            .collect();
-
-        let mut min_sum = i32::MAX;
-        let min_fuel = *data.iter().min().unwrap();
-        let max_fuel = *data.iter().max().unwrap();
-
-        for i in min_fuel..max_fuel {
-            let sum: i32 = data
-                .iter()
-                .map(|v| {
-                    let mut cost = 0;
-                    for j in 0..=(v - i).abs() {
-                        cost += j;
-                    }
-
-                    cost
-                })
-                .sum();
-            min_sum = min(sum, min_sum);
-        }
-
-        Ok(Box::new(min_sum))
+        Ok(Box::new(self.solve(input, |v, a| {
+            (1..=(v - a).abs()).fold(0, |sum, curr| sum + curr)
+        })))
     }
 }
 
@@ -81,6 +64,6 @@ mod tests {
             .part_2(Some(input.to_string()))
             .unwrap();
 
-        assert_eq!("", result.to_string())
+        assert_eq!("168", result.to_string())
     }
 }
