dcbd09274d28ea58ec14f0a4d9021365ed94c8fd
Vitali Stsepaniuk <contact@vitaliy.dev>
2021-12-16 10:26:52 +0100

    refactor: day16

--- src/solutions/day16.rs
+++ src/solutions/day16.rs
@@ -1,6 +1,4 @@
 use crate::solutions::{Result, Solution};
-use clap::Values;
-use itertools::Itertools;
 use std::fmt::Display;
 
 #[derive(Default)]
@@ -12,6 +10,21 @@ enum Packet {
     Op(usize, usize, Vec<Packet>),
 }
 
+trait BinString {
+    fn as_bits(&self) -> Vec<u8>;
+}
+
+impl BinString for str {
+    fn as_bits(&self) -> Vec<u8> {
+        self.chars()
+            .flat_map(|b| {
+                let digit = b.to_digit(16).unwrap();
+                (0..4).map(move |i| ((digit << (i + 1) & 0b10000) >> 4) as u8)
+            })
+            .collect::<Vec<_>>()
+    }
+}
+
 impl Packet {
     fn sum(&self) -> usize {
         match self {
@@ -21,13 +34,6 @@ impl Packet {
     }
 
     fn value(&self) -> usize {
-        if let Packet::Literal(_, _, value) = self {
-            print!("{} ", value);
-        }
-        // if let Packet::Op(_, n, _) = self {
-        //     print!("{}", n);
-        // }
-
         match self {
             Packet::Literal(_, _, value) => *value,
             Packet::Op(_, 0, sub) => sub.iter().map(|p| p.value()).sum(),
@@ -47,20 +53,7 @@ trait Bits {
     fn print_bits(&self);
 }
 
-impl Bits for Vec<u8> {
-    fn bits_take(&self, from: usize, n: usize) -> usize {
-        self[from..from + n]
-            .iter()
-            .fold(0, |r, b| (r << 1) + *b as usize)
-    }
-
-    fn print_bits(&self) {
-        self.iter().for_each(|x| print!("{}", x));
-        println!();
-    }
-}
-
-impl Bits for &[u8] {
+impl Bits for [u8] {
     fn bits_take(&self, from: usize, n: usize) -> usize {
         self[from..from + n]
             .iter()
@@ -79,15 +72,11 @@ impl DaySolution {
         let mut packets = vec![];
 
         while index < bits.len() && packets.len() < count {
-            println!();
-            dbg!(index);
             let version = bits.bits_take(index, 3);
             index += 3;
 
             let typ = bits.bits_take(index, 3);
             index += 3;
-
-            dbg!(version, typ);
 
             if typ == 4 {
                 let mut literals: Vec<usize> = vec![];
@@ -103,7 +92,6 @@ impl DaySolution {
                 literals.push(literal);
 
                 let value = literals.iter().fold(0, |r, b| (r << 4) + *b);
-                dbg!(value);
 
                 packets.push(Packet::Literal(version, typ, value as usize));
                 continue;
@@ -112,23 +100,17 @@ impl DaySolution {
             let op_type = bits[index];
             index += 1;
 
-            let op_len = if op_type == 0 {
-                bits.bits_take(index, 15) as usize
-            } else {
-                bits.bits_take(index, 11) as usize
-            };
-            dbg!(op_type, op_len);
-            //
-            index += if op_type == 0 { 15 } else { 11 };
+            let index_inc = if op_type == 0 { 15 } else { 11 };
+            let op_len = bits.bits_take(index, index_inc);
+            index += index_inc;
 
             if op_type == 0 {
-                // bits
                 let bits = bits
                     .to_vec()
                     .iter()
                     .skip(index)
                     .take(op_len)
-                    .map(|v| *v)
+                    .copied()
                     .collect::<Vec<u8>>();
 
                 let result = self.read_packets(&bits, usize::MAX);
@@ -140,7 +122,7 @@ impl DaySolution {
                     .to_vec()
                     .iter()
                     .skip(index)
-                    .map(|v| *v)
+                    .copied()
                     .collect::<Vec<u8>>();
 
                 let result = self.read_packets(&bits, op_len);
@@ -156,81 +138,39 @@ impl DaySolution {
 
 impl Solution for DaySolution {
     fn part_1(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
-        let encoded = input.unwrap();
-
-        let mut bits = encoded
-            .chars()
-            .flat_map(|b| {
-                let digit = b.to_digit(16).unwrap();
-                let mut digit = (digit & 0b1111) as u8;
-
-                let mut bits = vec![];
-                for _ in 0..4 {
-                    bits.push(digit & 1);
-
-                    digit = digit >> 1;
-                }
-
-                bits.reverse();
-                bits
-            })
-            .collect::<Vec<_>>();
-
-        let (_, packets) = self.read_packets(&bits, 1);
+        let (_, packets) = self.read_packets(&input.unwrap().as_bits(), 1);
         let sum = packets[0].sum();
 
         Ok(Box::new(sum))
     }
 
     fn part_2(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
-        let encoded = input.unwrap();
-
-        let mut bits = encoded
-            .chars()
-            .flat_map(|b| {
-                let digit = b.to_digit(16).unwrap();
-                let mut digit = (digit & 0b1111) as u8;
-
-                let mut bits = vec![];
-                for _ in 0..4 {
-                    bits.push(digit & 1);
-
-                    digit = digit >> 1;
-                }
-
-                bits.reverse();
-                bits
-            })
-            .collect::<Vec<_>>();
-
-        let (_, packets) = self.read_packets(&bits, 1);
+        let (_, packets) = self.read_packets(&input.unwrap().as_bits(), 1);
 
         Ok(Box::new(packets[0].value()))
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::day16::DaySolution;
-//     use crate::Solution;
-//
-//     #[test]
-//     fn part_1() {
-//         let input = include_str!("../../inputs/day16_demo.txt");
-//         let result = DaySolution::default()
-//             .part_1(Some(input.to_string()))
-//             .unwrap();
-//
-//         assert_eq!("", result.to_string())
-//     }
-//
-//     #[test]
-//     fn part_2() {
-//         let input = include_str!("../../inputs/day16_demo.txt");
-//         let result = DaySolution::default()
-//             .part_2(Some(input.to_string()))
-//             .unwrap();
-//
-//         assert_eq!("", result.to_string())
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use crate::day16::DaySolution;
+    use crate::Solution;
+
+    #[test]
+    fn part_1() {
+        let result = DaySolution::default()
+            .part_1(Some("8A004A801A8002F478".to_string()))
+            .unwrap();
+
+        assert_eq!("16", result.to_string());
+    }
+
+    #[test]
+    fn part_2() {
+        let result = DaySolution::default()
+            .part_2(Some("C200B40A82".to_string()))
+            .unwrap();
+
+        assert_eq!("3", result.to_string());
+    }
+}
