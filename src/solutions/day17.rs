bb99f58a7e999bbc2ada7dd7838e971bcae8dabb
Vitali Stsepaniuk <contact@vitaliy.dev>
2021-12-17 10:09:27 +0100

    refactor: day17

--- src/solutions/day17.rs
+++ src/solutions/day17.rs
@@ -1,9 +1,25 @@
 use crate::solutions::{Result, Solution};
+use itertools::iproduct;
+use regex::Regex;
 use std::fmt::Display;
-use std::ops::RangeInclusive;
 
 #[derive(Default)]
 pub struct DaySolution;
+
+impl DaySolution {
+    fn parse(&self, input: Option<String>) -> (i32, i32, i32, i32) {
+        let re = Regex::new(r"target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)").unwrap();
+        let input = input.unwrap();
+        let caps = re.captures(&input).unwrap();
+
+        (
+            caps.get(1).unwrap().as_str().parse().unwrap(),
+            caps.get(2).unwrap().as_str().parse().unwrap(),
+            caps.get(3).unwrap().as_str().parse().unwrap(),
+            caps.get(4).unwrap().as_str().parse().unwrap(),
+        )
+    }
+}
 
 impl DaySolution {
     fn hit(
@@ -17,23 +33,21 @@ impl DaySolution {
         let (mut vx, mut vy) = velocity;
         let (mut x, mut y) = (0, 0);
 
-        let mut yy = i32::MIN;
+        let mut max_y = 0;
+
         loop {
             x += vx;
             y += vy;
 
-            if vx > 0 {
-                vx -= 1;
-            }
-
+            vx -= vx.signum();
             vy -= 1;
 
-            if y > yy {
-                yy = y;
+            if y > max_y {
+                max_y = y;
             }
 
             if x >= x_min && x <= x_max && y >= y_min && y <= y_max {
-                return Some(yy);
+                return Some(max_y);
             }
 
             if x > x_max {
@@ -51,82 +65,63 @@ impl DaySolution {
 
 impl Solution for DaySolution {
     fn part_1(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
-        let input = input.unwrap();
-        let (_, input) = input.split_once(": ").unwrap();
-        let (x_range, y_range) = input.split_once(", ").unwrap();
+        let (x_min, x_max, y_min, y_max) = self.parse(input);
 
-        let x_range = x_range.replace("x=", "");
-        let y_range = y_range.replace("y=", "");
+        let max_height = iproduct!(0..=x_max, y_min..x_max)
+            .filter_map(|(dx, dy)| self.hit((dx, dy), x_min, x_max, y_min, y_max))
+            .max()
+            .unwrap();
 
-        let (x_min, x_max) = x_range.split_once("..").unwrap();
-        let (y_min, y_max) = y_range.split_once("..").unwrap();
-
-        let (x_min, x_max) = (x_min.parse::<i32>().unwrap(), x_max.parse::<i32>().unwrap());
-        let (y_min, y_max) = (y_min.parse::<i32>().unwrap(), y_max.parse::<i32>().unwrap());
-
-        let mut mm = i32::MIN;
-        for i in 0..=x_max {
-            for j in y_min..x_max {
-                if let Some(y) = self.hit((i, j), x_min, x_max, y_min, y_max) {
-                    if y > mm {
-                        mm = y;
-                    }
-                }
-            }
-        }
-
-        Ok(Box::new(mm))
+        Ok(Box::new(max_height))
     }
 
     fn part_2(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
-        let input = input.unwrap();
-        let (_, input) = input.split_once(": ").unwrap();
-        let (x_range, y_range) = input.split_once(", ").unwrap();
+        let (x_min, x_max, y_min, y_max) = self.parse(input);
 
-        let x_range = x_range.replace("x=", "");
-        let y_range = y_range.replace("y=", "");
-
-        let (x_min, x_max) = x_range.split_once("..").unwrap();
-        let (y_min, y_max) = y_range.split_once("..").unwrap();
-
-        let (x_min, x_max) = (x_min.parse::<i32>().unwrap(), x_max.parse::<i32>().unwrap());
-        let (y_min, y_max) = (y_min.parse::<i32>().unwrap(), y_max.parse::<i32>().unwrap());
-
-        let mut count = 0;
-        for i in 0..=x_max {
-            for j in y_min..x_max {
-                if let Some(y) = self.hit((i, j), x_min, x_max, y_min, y_max) {
-                    count += 1;
-                }
-            }
-        }
+        let count = iproduct!(0..=x_max, y_min..x_max)
+            .filter_map(|(dx, dy)| self.hit((dx, dy), x_min, x_max, y_min, y_max))
+            .count();
 
         Ok(Box::new(count))
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::day17::DaySolution;
-//     use crate::Solution;
-//
-//     #[test]
-//     fn part_1() {
-//         let input = include_str!("../../inputs/day17_demo.txt");
-//         let result = DaySolution::default()
-//             .part_1(Some(input.to_string()))
-//             .unwrap();
-//
-//         assert_eq!("", result.to_string())
-//     }
-//
-//     #[test]
-//     fn part_2() {
-//         let input = include_str!("../../inputs/day17_demo.txt");
-//         let result = DaySolution::default()
-//             .part_2(Some(input.to_string()))
-//             .unwrap();
-//
-//         assert_eq!("", result.to_string())
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use crate::day17::DaySolution;
+    use crate::Solution;
+
+    #[test]
+    fn part_1() {
+        let input = include_str!("../../inputs/day17_demo.txt");
+        let result = DaySolution::default()
+            .part_1(Some(input.to_string()))
+            .unwrap();
+
+        assert_eq!("45", result.to_string());
+
+        let input = include_str!("../../inputs/day17.txt");
+        let result = DaySolution::default()
+            .part_1(Some(input.to_string()))
+            .unwrap();
+
+        assert_eq!("5460", result.to_string());
+    }
+
+    #[test]
+    fn part_2() {
+        let input = include_str!("../../inputs/day17_demo.txt");
+        let result = DaySolution::default()
+            .part_2(Some(input.to_string()))
+            .unwrap();
+
+        assert_eq!("112", result.to_string());
+
+        let input = include_str!("../../inputs/day17.txt");
+        let result = DaySolution::default()
+            .part_2(Some(input.to_string()))
+            .unwrap();
+
+        assert_eq!("3618", result.to_string());
+    }
+}
