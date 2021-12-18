35e82635045bbd73e5cf525c74fb228fea0ea428
Vitali Stsepaniuk <contact@vitaliy.dev>
2021-12-18 07:54:02 +0100

    day18:

--- src/solutions/day18.rs
+++ src/solutions/day18.rs
@@ -5,37 +5,103 @@ use nom::character::complete::char;
 use nom::sequence::{preceded, separated_pair, terminated};
 use nom::{combinator::map_res, IResult};
 use std::fmt::Display;
-use crate::day18::SnailFish::Pair;
+use trees::{Node, Tree};
 
-// #[derive(Debug)]
-// enum SnailFishPart {
-//     Value(i32),
-//     Array(Vec<SnailFish2>),
-// }
-//
-// type SnailFish2 = (SnailFishPart, SnailFishPart);
-
-#[derive(Debug, Clone)]
+#[derive(Debug, PartialEq, Eq)]
 enum SnailFish {
-
-    Pair(Box<SnailFish>, Box<SnailFish>),
-    Value(i32),
+    Pair1(i32, i32),
+    Pair2(i32, Box<SnailFish>),
+    Pair3(Box<SnailFish>, i32),
+    Pair4(Box<SnailFish>, Box<SnailFish>),
 }
 
 impl SnailFish {
-    // fn add(&self) -> SnailFish {}
+    fn tree(&self) -> Tree<i32> {
+        let mut tree = Tree::new(0);
+
+        match self {
+            SnailFish::Pair1(a, b) => {
+                tree.root_mut().push_back(Tree::new(*a));
+                tree.root_mut().push_back(Tree::new(*b));
+            }
+            SnailFish::Pair2(a, b) => {
+                tree.root_mut().push_back(Tree::new(*a));
+                tree.root_mut().push_back(b.tree());
+            }
+            SnailFish::Pair3(a, b) => {
+                tree.root_mut().push_back(a.tree());
+                tree.root_mut().push_back(Tree::new(*b));
+            }
+            SnailFish::Pair4(a, b) => {
+                tree.root_mut().push_back(a.tree());
+                tree.root_mut().push_back(b.tree());
+            }
+        }
+
+        tree
+
+        // root.append()
+        // match self {
+        //     SnailFish::Pair1(a, b) => {
+        //     }
+        //     SnailFish::Pair2(_, _) => {}
+        //     SnailFish::Pair3(_, _) => {}
+        //     SnailFish::Pair4(_, _) => {}
+        // }
+    }
 
     fn reduce(&self, level: usize) -> SnailFish {
-        // [[[[[9,8],1],2],3],4] -> [[[[0,9],2],3],4]
         match self {
-            Self::Pair(a, b) if level < 4 => {
-                Self::Pair(Box::new(a.reduce(level + 1)), Box::new(b.reduce(level + 1)))
+            Self::Pair1(a, b) => SnailFish::Pair1(*a, *b),
+            Self::Pair2(a, b) if level >= 4 => {
+                if let Self::Pair1(c, d) = **b {
+                    SnailFish::Pair1(a + c, 0)
+                } else {
+                    SnailFish::Pair2(*a, Box::new(b.reduce(level + 1)))
+                }
             }
-            Self::Pair(a, b) if level > 4 => {
-                unimplemented!();
-                if let Self::Value(a) =
+            Self::Pair2(a, b) => SnailFish::Pair2(*a, Box::new(b.reduce(level + 1))),
+            Self::Pair3(a, b) if level >= 4 => {
+                if let Self::Pair1(c, d) = **a {
+                    SnailFish::Pair1(0, d + b)
+                } else {
+                    SnailFish::Pair3(Box::new(a.reduce(level + 1)), *b)
+                }
             }
-            _ => self,
+            Self::Pair3(a, b) => SnailFish::Pair3(Box::new(a.reduce(level + 1)), *b),
+            Self::Pair4(a, b) => {
+                SnailFish::Pair4(Box::new(a.reduce(level + 1)), Box::new(b.reduce(level + 1)))
+            }
+        }
+    }
+
+    fn debug(&self) {
+        self.print(0);
+    }
+
+    fn print(&self, level: usize) {
+        match self {
+            SnailFish::Pair1(a, b) => print!("[{},{}]", a, b),
+            SnailFish::Pair2(a, b) => {
+                print!("[{},", a);
+                b.print(level + 1);
+                print!("]");
+            }
+            SnailFish::Pair3(a, b) => {
+                print!("[");
+                a.print(level + 1);
+                print!(",{}]", b);
+            }
+            SnailFish::Pair4(a, b) => {
+                print!("[");
+                a.print(level + 1);
+                print!(",");
+                b.print(level + 1);
+                print!("]");
+            }
+        }
+        if level == 0 {
+            println!();
         }
     }
 }
@@ -64,10 +130,7 @@ impl Parser {
             ),
         )(input)?;
 
-        Ok((
-            input,
-            SnailFish::Pair(Box::new(SnailFish::Value(a)), Box::new(SnailFish::Value(b))),
-        ))
+        Ok((input, SnailFish::Pair1(a, b)))
     }
 
     fn pair2(input: &str) -> IResult<&str, SnailFish> {
@@ -79,10 +142,7 @@ impl Parser {
             ),
         )(input)?;
 
-        Ok((
-            input,
-            SnailFish::Pair(Box::new(SnailFish::Value(a)), Box::new(b)),
-        ))
+        Ok((input, SnailFish::Pair2(a, Box::new(b))))
     }
 
     fn pair3(input: &str) -> IResult<&str, SnailFish> {
@@ -94,10 +154,7 @@ impl Parser {
             ),
         )(input)?;
 
-        Ok((
-            input,
-            SnailFish::Pair(Box::new(a), Box::new(SnailFish::Value(b))),
-        ))
+        Ok((input, SnailFish::Pair3(Box::new(a), b)))
     }
 
     fn pair4(input: &str) -> IResult<&str, SnailFish> {
@@ -109,7 +166,7 @@ impl Parser {
             ),
         )(input)?;
 
-        Ok((input, SnailFish::Pair(Box::new(a), Box::new(b))))
+        Ok((input, SnailFish::Pair4(Box::new(a), Box::new(b))))
     }
 
     fn value(input: &str) -> IResult<&str, i32> {
@@ -127,15 +184,32 @@ impl Parser {
 
 impl Solution for DaySolution {
     fn part_1(&mut self, _input: Option<String>) -> Result<Box<dyn Display>> {
-        println!("{:?}", Parser::parse("[1,2]"));
-        println!("{:?}", Parser::parse("[[1,2],3]"));
-        println!("{:?}", Parser::parse("[9,[8,7]]"));
-        println!("{:?}", Parser::parse("[[1,9],[8,5]]"));
-        println!("{:?}", Parser::parse("[[[[1,2],[3,4]],[[5,6],[7,8]]],9]"));
-        println!(
-            "{:?}",
-            Parser::parse("[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]")
-        );
+        // println!("{:?}", Parser::parse("[1,2]"));
+        // println!("{:?}", Parser::parse("[[1,2],3]"));
+        // println!("{:?}", Parser::parse("[9,[8,7]]"));
+        // println!("{:?}", Parser::parse("[[1,9],[8,5]]"));
+        // println!("{:?}", Parser::parse("[[[[1,2],[3,4]],[[5,6],[7,8]]],9]"));
+        // println!(
+        //     "{:?}",
+        //     Parser::parse("[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]")
+        // );
+
+        println!("{:?}", Parser::parse("[1,2]").unwrap().tree().to_string());
+
+        let f = Parser::parse("[[[[[9,8],1],2],3],4]").unwrap();
+        // [[[[0,9],2],3],4]
+        // f.debug();
+        // f.reduce(1).debug();
+
+        println!("{:?}", f.tree().to_string());
+
+        let f = Parser::parse("[7,[6,[5,[4,[3,2]]]]]").unwrap();
+        // f.debug();
+        // f.reduce(1).debug();
+
+        let f = Parser::parse("[[6,[5,[4,[3,2]]]],1]").unwrap();
+        // f.debug();
+        // f.reduce(1).debug();
 
         Ok(Box::new(1))
     }
