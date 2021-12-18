a626e1c2a0f315b8df4676e1bb396ad5ed6fb881
Vitali Stsepaniuk <contact@vitaliy.dev>
2021-12-18 18:31:51 +0100

    day18: rust

--- src/solutions/day18.rs
+++ src/solutions/day18.rs
@@ -1,254 +1,267 @@
+use crate::helpers::parse_lines;
 use crate::solutions::{Result, Solution};
+use itertools::{iproduct, Itertools};
+use json::JsonValue;
 use nom::branch::alt;
-use nom::bytes::streaming::take_while1;
-use nom::character::complete::char;
-use nom::sequence::{preceded, separated_pair, terminated};
+use nom::bytes::complete::tag;
+use nom::character::complete::digit1;
+use nom::combinator::map;
+use nom::multi::many1;
 use nom::{combinator::map_res, IResult};
-use std::collections::VecDeque;
-use std::fmt::Display;
-use trees::{Node, Tree, TreeWalk};
-
-#[derive(Debug, PartialEq, Eq)]
-enum SnailFish {
-    Pair1(i32, i32),
-    Pair2(i32, Box<SnailFish>),
-    Pair3(Box<SnailFish>, i32),
-    Pair4(Box<SnailFish>, Box<SnailFish>),
-}
-
-impl SnailFish {
-    fn tree(&self) -> Tree<(i32, usize)> {
-        self._tree(0)
-    }
-
-    fn _tree(&self, depth: usize) -> Tree<(i32, usize)> {
-        let mut tree = Tree::new((-1, depth));
-
-        match self {
-            SnailFish::Pair1(a, b) => {
-                tree.root_mut().push_back(Tree::new((*a, depth)));
-                tree.root_mut().push_back(Tree::new((*b, depth)));
-            }
-            SnailFish::Pair2(a, b) => {
-                tree.root_mut().push_back(Tree::new((*a, depth)));
-                tree.root_mut().push_back(b._tree(depth + 1));
-            }
-            SnailFish::Pair3(a, b) => {
-                tree.root_mut().push_back(a._tree(depth + 1));
-                tree.root_mut().push_back(Tree::new((*b, depth)));
-            }
-            SnailFish::Pair4(a, b) => {
-                tree.root_mut().push_back(a._tree(depth + 1));
-                tree.root_mut().push_back(b._tree(depth + 1));
-            }
-        }
-
-        tree
-    }
-
-    // fn add_to_leftmost(self, value: i32) -> Self {
-    //     match self {
-    //         Self::Value(n) => Self::Value(n + value),
-    //         Self::Pair2(l, r) => Self::Pair2(Box::new(l.add_to_leftmost(val)), r),
-    //     }
-    // }
-    //
-    // fn add_to_rightmost(self, val: u64) -> Self {
-    //     match self {
-    //         Self::Regular(n) => Number::Regular(n + val),
-    //         Number::Pair(l, r) => Number::Pair(l, Box::new(r.add_to_rightmost(val))),
-    //     }
-    // }
-
-    fn reduce(&self, level: usize) -> Self {
-        println!("{:?} {:?}", level, self);
-        match self {
-            Self::Pair1(a, b) => SnailFish::Pair1(*a, *b),
-            Self::Pair2(a, b) if level >= 4 => {
-                if let Self::Pair1(c, d) = **b {
-                    SnailFish::Pair1(a + c, 0)
-                } else {
-                    SnailFish::Pair2(*a, Box::new(b.reduce(level + 1)))
-                }
-            }
-            Self::Pair2(a, b) => SnailFish::Pair2(*a, Box::new(b.reduce(level + 1))),
-            Self::Pair3(a, b) if level >= 4 => {
-                if let Self::Pair1(c, d) = **a {
-                    SnailFish::Pair1(0, d + b)
-                } else {
-                    SnailFish::Pair3(Box::new(a.reduce(level + 1)), *b)
-                }
-            }
-            Self::Pair3(a, b) => SnailFish::Pair3(Box::new(a.reduce(level + 1)), *b),
-            Self::Pair4(a, b) => {
-                SnailFish::Pair4(Box::new(a.reduce(level + 1)), Box::new(b.reduce(level + 1)))
-            }
-        }
-    }
-
-    fn explode(&self) {
-        let mut tree = self.tree();
-        let mut walk = TreeWalk::from(tree);
-
-        while let Some(visit) = walk.get() {
-            let node = visit.node();
-            let (data, depth) = *node.data();
-
-            if data == -1 && depth == 4 {
-                let left = *walk.get().unwrap().node().data();
-                walk.forward();
-                let right = *walk.get().unwrap().node().data();
-                walk.forward();
-
-                // println!("LEFT: {:?}, RIGHT: {:?}", *left, *right);
-                break;
-            }
-            // visit.node()
-            println!("{} {:?} ({})", " ".repeat(depth), data, depth);
-
-            walk.forward();
-        }
-
-        // self._explode(&mut tree, 0);
-    }
-
-    fn _explode(&self, root: &mut Tree<i32>, level: usize) {
-        let mut iter = root.iter();
-        let left = iter.next();
-        let right = iter.next();
-    }
-
-    fn to_string(&self) -> String {
-        match self {
-            SnailFish::Pair1(a, b) => format!("[{},{}]", a, b),
-            SnailFish::Pair2(a, b) => format!("[{},{}]", a, b.to_string()),
-            SnailFish::Pair3(a, b) => format!("[{},{}]", a.to_string(), b),
-            SnailFish::Pair4(a, b) => format!("[{},{}]", a.to_string(), b.to_string()),
-        }
-    }
-
-    fn reduce_tree(&self, tree: &Tree<i32>) {
-        for node in tree.iter() {
-            println!("{:?} {:?}", node.data(), node.node_count());
-        }
-    }
-}
-
-impl Default for SnailFish {
-    fn default() -> Self {
-        unimplemented!()
-    }
-}
-
-trait ToSnailfish {
-    fn to_snailfish(&self) -> SnailFish;
-}
-
-impl ToSnailfish for str {
-    fn to_snailfish(&self) -> SnailFish {
-        Parser::parse(&self).unwrap()
-    }
-}
+use std::fmt::{Debug, Display};
 
 #[derive(Default)]
 pub struct DaySolution;
 
-struct Parser;
-impl Parser {
-    fn parse_int(input: &str) -> std::result::Result<i32, std::num::ParseIntError> {
-        i32::from_str_radix(input, 10)
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum Item {
+    Open,
+    Close,
+    Comma,
+    Value(usize),
+}
+
+impl Item {
+    fn get_value(&self) -> usize {
+        match self {
+            Self::Value(value) => *value,
+            _ => unreachable!(),
+        }
+    }
+}
+
+fn parse_usize(input: &str) -> std::result::Result<usize, std::num::ParseIntError> {
+    input.parse::<usize>()
+}
+
+impl Item {
+    fn comma(input: &str) -> IResult<&str, Item> {
+        map(tag(","), |_| Self::Comma)(input)
     }
 
-    fn pair1(input: &str) -> IResult<&str, SnailFish> {
-        let (input, (a, b)) = preceded(
-            char('['),
-            terminated(
-                separated_pair(Self::value, char(','), Self::value),
-                char(']'),
-            ),
-        )(input)?;
-
-        Ok((input, SnailFish::Pair1(a, b)))
+    fn open(input: &str) -> IResult<&str, Item> {
+        map(tag("["), |_| Self::Open)(input)
     }
 
-    fn pair2(input: &str) -> IResult<&str, SnailFish> {
-        let (input, (a, b)) = preceded(
-            char('['),
-            terminated(
-                separated_pair(Self::value, char(','), Self::snailfish),
-                char(']'),
-            ),
-        )(input)?;
-
-        Ok((input, SnailFish::Pair2(a, Box::new(b))))
+    fn close(input: &str) -> IResult<&str, Item> {
+        map(tag("]"), |_| Self::Close)(input)
     }
 
-    fn pair3(input: &str) -> IResult<&str, SnailFish> {
-        let (input, (a, b)) = preceded(
-            char('['),
-            terminated(
-                separated_pair(Self::snailfish, char(','), Self::value),
-                char(']'),
-            ),
-        )(input)?;
-
-        Ok((input, SnailFish::Pair3(Box::new(a), b)))
+    fn value(input: &str) -> IResult<&str, Item> {
+        map(map_res(digit1, parse_usize), Self::Value)(input)
     }
 
-    fn pair4(input: &str) -> IResult<&str, SnailFish> {
-        let (input, (a, b)) = preceded(
-            char('['),
-            terminated(
-                separated_pair(Self::snailfish, char(','), Self::snailfish),
-                char(']'),
-            ),
-        )(input)?;
+    fn any(input: &str) -> IResult<&str, Item> {
+        alt((Self::open, Self::close, Self::comma, Self::value))(input)
+    }
+}
 
-        Ok((input, SnailFish::Pair4(Box::new(a), Box::new(b))))
+trait Snailfish {
+    fn from_string(input: &str) -> Vec<Item>;
+    fn to_string(&self) -> String;
+    fn explode(&self) -> Vec<Item>;
+    fn split(&self) -> Vec<Item>;
+    fn add(&self, other: Vec<Item>) -> Vec<Item>;
+    fn reduce(&self) -> Vec<Item>;
+}
+
+impl Snailfish for Vec<Item> {
+    fn from_string(input: &str) -> Vec<Item> {
+        many1(Item::any)(input).unwrap_or_default().1
     }
 
-    fn value(input: &str) -> IResult<&str, i32> {
-        map_res(take_while1(|ch: char| ch.is_digit(10)), Self::parse_int)(input)
+    fn to_string(&self) -> String {
+        self.iter()
+            .map(|i| match i {
+                Item::Open => "[".to_string(),
+                Item::Close => "]".to_string(),
+                Item::Comma => ",".to_string(),
+                Item::Value(value) => format!("{}", value),
+            })
+            .join("")
     }
 
-    fn snailfish(input: &str) -> IResult<&str, SnailFish> {
-        alt((Parser::pair1, Parser::pair2, Parser::pair3, Parser::pair4))(input)
+    fn explode(&self) -> Vec<Item> {
+        let mut open_count = 0;
+        let mut explode_position = None;
+
+        for (index, item) in self.iter().enumerate() {
+            open_count += match item {
+                Item::Open => 1,
+                Item::Close => -1,
+                _ => 0,
+            };
+
+            if open_count == 5 {
+                explode_position = Some(index);
+                break;
+            }
+        }
+
+        if explode_position.is_none() {
+            return self.to_vec();
+        }
+
+        let explode_position = explode_position.unwrap();
+
+        let left = self[explode_position + 1].get_value();
+        let right = self[explode_position + 3].get_value();
+
+        let mut result = vec![Item::Value(0)];
+        let mut added = false;
+
+        let mut index = explode_position - 1;
+        loop {
+            match self[index] {
+                Item::Value(value) if !added => {
+                    result.insert(0, Item::Value(value + left));
+                    added = true;
+                }
+                _ => result.insert(0, self[index]),
+            };
+
+            if index == 0 {
+                break;
+            }
+            index -= 1;
+        }
+
+        index = explode_position + 5;
+        added = false;
+
+        while index < self.len() {
+            match self[index] {
+                Item::Value(value) if !added => {
+                    result.push(Item::Value(value + right));
+                    added = true;
+                }
+                _ => result.push(self[index]),
+            };
+            index += 1;
+        }
+
+        result
     }
 
-    fn parse(input: &str) -> Option<SnailFish> {
-        Self::snailfish(&input).map(|(_, snailfish)| snailfish).ok()
+    fn split(&self) -> Vec<Item> {
+        let mut has_split = false;
+
+        self.iter()
+            .flat_map(|item| match item {
+                Item::Value(value) if *value > 9 && !has_split => {
+                    has_split = true;
+
+                    let a = value / 2;
+                    let b = value - a;
+
+                    vec![
+                        Item::Open,
+                        Item::Value(a),
+                        Item::Comma,
+                        Item::Value(b),
+                        Item::Close,
+                    ]
+                }
+                _ => vec![*item],
+            })
+            .collect()
+    }
+
+    fn add(&self, other: Vec<Item>) -> Vec<Item> {
+        let mut result = vec![Item::Open];
+        result.extend(self);
+        result.push(Item::Comma);
+        result.extend(other);
+        result.push(Item::Close);
+
+        result
+    }
+
+    fn reduce(&self) -> Vec<Item> {
+        let mut last = self.clone();
+        loop {
+            let mut last_explode = last.clone();
+            loop {
+                let tmp = last_explode.explode();
+                if tmp == last_explode {
+                    last = tmp;
+                    break;
+                }
+
+                last_explode = tmp;
+            }
+
+            last_explode = last_explode.split();
+            if last_explode == last {
+                break;
+            }
+
+            last = last_explode
+        }
+
+        last
+    }
+}
+
+impl DaySolution {
+    fn magnitude(&self, value: JsonValue) -> f64 {
+        match value {
+            JsonValue::Array(arr) => {
+                let a = arr[0].clone();
+                let b = arr[1].clone();
+
+                match (a, b) {
+                    (JsonValue::Number(a), JsonValue::Number(b)) => {
+                        let a: f64 = a.into();
+                        let b: f64 = b.into();
+                        a * 3.0 + b * 2.0
+                    }
+                    (a, JsonValue::Number(b)) => {
+                        let b: f64 = b.into();
+                        self.magnitude(a) + b * 2.0
+                    }
+                    (JsonValue::Number(a), b) => {
+                        let a: f64 = a.into();
+                        a * 3.0 + self.magnitude(b)
+                    }
+                    (a, b) => self.magnitude(a) * 3.0 + self.magnitude(b) * 2.0,
+                }
+            }
+            _ => 0.0,
+        }
     }
 }
 
 impl Solution for DaySolution {
-    fn part_1(&mut self, _input: Option<String>) -> Result<Box<dyn Display>> {
-        // let f = Parser::parse("[7,[6,[5,[4,[3,2]]]]]").unwrap();
-        // f.debug();
-        // f.reduce(1).debug();
+    fn part_1(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
+        let lines = parse_lines::<String>(input);
+        let mut current = Vec::<Item>::from_string(&lines[0]);
 
-        let f = Parser::parse("[[[[[9,8],1],2],3],4]").unwrap();
-        // println!("{:?}", f.reduce(1).to_string());
-        f.explode();
+        for line in lines.iter().skip(1) {
+            current = current.add(Vec::from_string(line));
+            current = current.reduce();
+        }
 
-        // let f = Parser::parse("[7,[6,[5,[4,[3,2]]]]]").unwrap();
-        // println!("{:?}", f.reduce(1).to_string());
-        //
-        // let f = Parser::parse("[[6,[5,[4,[3,2]]]],1]").unwrap();
-        // println!("{:?}", f.reduce(1).to_string());
+        let value = json::parse(&current.to_string()).unwrap();
 
-        // let f = Parser::parse("[[6,[5,[4,[3,2]]]],1]").unwrap();
-        // f.explode();
-        // // println!("{:?}", f);
-        // let tree = f.tree();
-        // println!("{:?}", tree.to_string());
-        //
-        // f.reduce_tree(&tree);
-
-        Ok(Box::new(1))
+        Ok(Box::new(self.magnitude(value) as usize))
     }
 
-    fn part_2(&mut self, _input: Option<String>) -> Result<Box<dyn Display>> {
-        todo!()
+    fn part_2(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
+        let lines = parse_lines::<String>(input);
+
+        let max = iproduct!(lines.iter(), lines.iter().skip(1))
+            .map(|(a, b)| {
+                let a = Vec::from_string(a);
+                let b = Vec::from_string(b);
+
+                let json = json::parse(&a.add(b).reduce().to_string()).unwrap();
+                self.magnitude(json) as usize
+            })
+            .max()
+            .unwrap();
+
+        Ok(Box::new(max))
     }
 }
 
