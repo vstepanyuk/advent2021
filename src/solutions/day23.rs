d6a383f13b7f9acb368ce415eee40f322e8b67a2
Vitali Stsepaniuk <contact@vitaliy.dev>
2021-12-23 21:31:19 +0100

    day23 part2

--- src/solutions/day23.rs
+++ src/solutions/day23.rs
@@ -1,12 +1,24 @@
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 
+use lazy_static::lazy_static;
 use pathfinding::prelude::dijkstra;
 
 use crate::solutions::{Result, Solution};
 
 #[derive(Default)]
 pub struct DaySolution;
+
+lazy_static! {
+    static ref COSTS: HashMap::<char, usize> = {
+        let mut costs = HashMap::new();
+        costs.insert('A', 1usize);
+        costs.insert('B', 10usize);
+        costs.insert('C', 100usize);
+        costs.insert('D', 1000usize);
+        costs
+    };
+}
 
 type Room = (char, char);
 type Room2 = (char, char, char, char);
@@ -17,6 +29,7 @@ struct State {
     hallway: [char; 11],
 }
 
+#[derive(Eq, PartialEq, Debug, Hash, Copy, Clone)]
 struct State2 {
     rooms: [Room2; 4],
     hallway: [char; 11],
@@ -24,11 +37,6 @@ struct State2 {
 
 impl Display for State {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        // #############
-        // #...........#
-        // ###B#C#B#D###
-        //   #A#D#C#A#
-        // #############
         writeln!(f, "#############")?;
         let hallway = self.hallway.iter().collect::<String>();
         writeln!(f, "#{}#", hallway)?;
@@ -48,14 +56,39 @@ impl Display for State {
     }
 }
 
+impl Display for State2 {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "#############")?;
+        let hallway = self.hallway.iter().collect::<String>();
+        writeln!(f, "#{}#", hallway)?;
+        writeln!(
+            f,
+            "###{}#{}#{}#{}###",
+            self.rooms[0].0, self.rooms[1].0, self.rooms[2].0, self.rooms[3].0,
+        )?;
+        writeln!(
+            f,
+            "  #{}#{}#{}#{}#",
+            self.rooms[0].1, self.rooms[1].1, self.rooms[2].1, self.rooms[3].1,
+        )?;
+        writeln!(
+            f,
+            "  #{}#{}#{}#{}#",
+            self.rooms[0].2, self.rooms[1].2, self.rooms[2].2, self.rooms[3].2,
+        )?;
+        writeln!(
+            f,
+            "  #{}#{}#{}#{}#",
+            self.rooms[0].3, self.rooms[1].3, self.rooms[2].3, self.rooms[3].3,
+        )?;
+        writeln!(f, "  #########")?;
+
+        Ok(())
+    }
+}
+
 impl DaySolution {
     fn solve1(&self, start_state: [Room; 4]) {
-        let mut costs = HashMap::new();
-        costs.insert('A', 1usize);
-        costs.insert('B', 10usize);
-        costs.insert('C', 100usize);
-        costs.insert('D', 1000usize);
-
         let winning_room = [('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D')];
 
         let state = State {
@@ -92,7 +125,7 @@ impl DaySolution {
                         let mut new_state = state.clone();
                         new_state.rooms[room_index] = (*am2, '.');
 
-                        let cost = *costs.get(am2).unwrap();
+                        let cost = *COSTS.get(am2).unwrap();
                         new_states.push((new_state, cost));
 
                         continue;
@@ -116,7 +149,7 @@ impl DaySolution {
                         new_state.rooms[room_index].0 = '.';
 
                         let distance = self.distance(room_index, move_to);
-                        let cost = *costs.get(am1).unwrap();
+                        let cost = *COSTS.get(am1).unwrap();
 
                         new_states.push((new_state, cost * distance));
                     }
@@ -144,20 +177,6 @@ impl DaySolution {
                     };
 
                     let room_hallway_index = (room_index + 1) * 2;
-
-                    let (am1, am2) = state.rooms[room_index];
-                    if am1 != '.' {
-                        if am2 == '.' {
-                            let mut new_state = state.clone();
-                            new_state.rooms[room_index] = ('.', am1);
-
-                            let cost = *costs.get(&am1).unwrap();
-                            new_states.push((new_state, cost));
-                        }
-
-                        continue;
-                    }
-
                     let can_move = if move_from < room_hallway_index {
                         (move_from + 1..=room_hallway_index)
                             .all(|index| state.hallway[index] == '.')
@@ -174,12 +193,25 @@ impl DaySolution {
                         continue;
                     }
 
+                    let (am1, am2) = state.rooms[room_index];
+                    if am1 != '.' {
+                        if am2 == '.' {
+                            let mut new_state = state.clone();
+                            new_state.rooms[room_index] = ('.', am1);
+
+                            let cost = *COSTS.get(&am1).unwrap();
+                            new_states.push((new_state, cost));
+                        }
+
+                        continue;
+                    }
+
                     let mut new_state = state.clone();
                     new_state.rooms[room_index] = (am, am2);
                     new_state.hallway[move_from] = '.';
 
                     let distance = self.distance(room_index, move_from);
-                    let cost = *costs.get(&am).unwrap();
+                    let cost = *COSTS.get(&am).unwrap();
                     new_states.push((new_state, cost * distance));
                 }
 
@@ -201,16 +233,21 @@ impl DaySolution {
         println!("{}", cost);
     }
 
-    fn solve2(&self, start_state: [Room; 4]) {
+    fn solve2(&self, start_state: [Room2; 4]) {
         let mut costs = HashMap::new();
         costs.insert('A', 1usize);
         costs.insert('B', 10usize);
         costs.insert('C', 100usize);
         costs.insert('D', 1000usize);
 
-        let winning_room = [('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D')];
+        let winning_room = [
+            ('A', 'A', 'A', 'A'),
+            ('B', 'B', 'B', 'B'),
+            ('C', 'C', 'C', 'C'),
+            ('D', 'D', 'D', 'D'),
+        ];
 
-        let state = State {
+        let state = State2 {
             rooms: start_state,
             hallway: ['.'; 11], // Empty
         };
@@ -219,7 +256,7 @@ impl DaySolution {
             &state,
             |state| {
                 // println!("Current state\n{}", state);
-                let mut new_states: Vec<(State, usize)> = vec![];
+                let mut new_states: Vec<(State2, usize)> = vec![];
 
                 assert_eq!(state.hallway[2], '.');
                 assert_eq!(state.hallway[4], '.');
@@ -228,25 +265,12 @@ impl DaySolution {
 
                 for (room_index, room) in state.rooms.iter().enumerate() {
                     if winning_room[room_index] == *room {
-                        // println!("Winning room\n{}", state);
-
                         continue;
                     }
 
-                    let (am1, am2) = room;
+                    let (am1, am2, am3, am4) = room;
 
-                    if *am1 == '.' && *am2 == '.' {
-                        // println!("Empty room\n{}", state);
-                        continue;
-                    }
-
-                    if *am1 == '.' {
-                        let mut new_state = state.clone();
-                        new_state.rooms[room_index] = (*am2, '.');
-
-                        let cost = *costs.get(am2).unwrap();
-                        new_states.push((new_state, cost));
-
+                    if *am1 == '.' && *am2 == '.' && *am3 == '.' && *am4 == '.' {
                         continue;
                     }
 
@@ -262,15 +286,52 @@ impl DaySolution {
                             continue;
                         }
 
-                        let mut new_state = state.clone();
+                        match (am1, am2, am3, am4) {
+                            ('.', '.', '.', am) => {
+                                let mut new_state = state.clone();
 
-                        new_state.hallway[move_to] = *am1;
-                        new_state.rooms[room_index].0 = '.';
+                                new_state.hallway[move_to] = *am;
+                                new_state.rooms[room_index].3 = '.';
 
-                        let distance = self.distance(room_index, move_to);
-                        let cost = *costs.get(am1).unwrap();
+                                let distance = self.distance(room_index, move_to) + 3;
+                                let cost = *COSTS.get(am).unwrap();
 
-                        new_states.push((new_state, cost * distance));
+                                new_states.push((new_state, cost * distance));
+                            }
+                            ('.', '.', am, _) => {
+                                let mut new_state = state.clone();
+
+                                new_state.hallway[move_to] = *am;
+                                new_state.rooms[room_index].2 = '.';
+
+                                let distance = self.distance(room_index, move_to) + 2;
+                                let cost = *COSTS.get(am).unwrap();
+
+                                new_states.push((new_state, cost * distance));
+                            }
+                            ('.', am, _, _) => {
+                                let mut new_state = state.clone();
+
+                                new_state.hallway[move_to] = *am;
+                                new_state.rooms[room_index].1 = '.';
+
+                                let distance = self.distance(room_index, move_to) + 1;
+                                let cost = *COSTS.get(am).unwrap();
+
+                                new_states.push((new_state, cost * distance));
+                            }
+                            (am, _, _, _) => {
+                                let mut new_state = state.clone();
+
+                                new_state.hallway[move_to] = *am;
+                                new_state.rooms[room_index].0 = '.';
+
+                                let distance = self.distance(room_index, move_to);
+                                let cost = *COSTS.get(am).unwrap();
+
+                                new_states.push((new_state, cost * distance));
+                            }
+                        }
                     }
                 }
 
@@ -297,19 +358,6 @@ impl DaySolution {
 
                     let room_hallway_index = (room_index + 1) * 2;
 
-                    let (am1, am2) = state.rooms[room_index];
-                    if am1 != '.' {
-                        if am2 == '.' {
-                            let mut new_state = state.clone();
-                            new_state.rooms[room_index] = ('.', am1);
-
-                            let cost = *costs.get(&am1).unwrap();
-                            new_states.push((new_state, cost));
-                        }
-
-                        continue;
-                    }
-
                     let can_move = if move_from < room_hallway_index {
                         (move_from + 1..=room_hallway_index)
                             .all(|index| state.hallway[index] == '.')
@@ -317,22 +365,56 @@ impl DaySolution {
                         (room_hallway_index..move_from).all(|index| state.hallway[index] == '.')
                     };
 
-                    if is_debug {
-                        println!("DEBUG {} {}", can_move, am);
-                        println!("{} {}", room_hallway_index, move_from);
-                    }
-
                     if !can_move {
                         continue;
                     }
 
-                    let mut new_state = state.clone();
-                    new_state.rooms[room_index] = (am, am2);
-                    new_state.hallway[move_from] = '.';
+                    let (am1, am2, am3, am4) = state.rooms[room_index];
 
-                    let distance = self.distance(room_index, move_from);
-                    let cost = *costs.get(&am).unwrap();
-                    new_states.push((new_state, cost * distance));
+                    match (am1, am2, am3, am4) {
+                        ('.', '.', '.', '.') => {
+                            let mut new_state = state.clone();
+                            new_state.hallway[move_from] = '.';
+                            new_state.rooms[room_index] = ('.', '.', '.', am);
+
+                            let distance = self.distance(room_index, move_from) + 3;
+                            let cost = *COSTS.get(&am).unwrap();
+                            new_states.push((new_state, cost * distance));
+                        }
+                        ('.', '.', '.', _) => {
+                            let mut new_state = state.clone();
+                            new_state.hallway[move_from] = '.';
+                            new_state.rooms[room_index] = ('.', '.', am, am4);
+
+                            let distance = self.distance(room_index, move_from) + 2;
+                            let cost = *COSTS.get(&am).unwrap();
+                            new_states.push((new_state, cost * distance));
+                        }
+                        ('.', '.', _, _) => {
+                            let mut new_state = state.clone();
+                            new_state.hallway[move_from] = '.';
+                            new_state.rooms[room_index] = ('.', am, am3, am4);
+
+                            let distance = self.distance(room_index, move_from) + 1;
+                            let cost = *COSTS.get(&am).unwrap();
+                            new_states.push((new_state, cost * distance));
+                        }
+                        ('.', _, _, _) => {
+                            let mut new_state = state.clone();
+                            new_state.hallway[move_from] = '.';
+                            new_state.rooms[room_index] = (am, am2, am3, am4);
+
+                            let distance = self.distance(room_index, move_from);
+                            let cost = *COSTS.get(&am).unwrap();
+                            new_states.push((new_state, cost * distance));
+                        }
+                        _ => continue,
+                    }
+
+                    if is_debug {
+                        println!("DEBUG {} {}", can_move, am);
+                        println!("{} {}", room_hallway_index, move_from);
+                    }
                 }
 
                 if is_debug {
@@ -389,14 +471,19 @@ impl DaySolution {
 }
 
 impl Solution for DaySolution {
-    fn part_1(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
+    fn part_1(&mut self, _input: Option<String>) -> Result<Box<dyn Display>> {
         self.solve1([('A', 'D'), ('C', 'D'), ('B', 'B'), ('A', 'C')]);
 
         Ok(Box::new(1))
     }
 
     fn part_2(&mut self, _input: Option<String>) -> Result<Box<dyn Display>> {
-        self.solve1([('A', 'D'), ('C', 'D'), ('B', 'B'), ('A', 'C')]);
+        self.solve2([
+            ('A', 'D', 'D', 'D'),
+            ('C', 'C', 'B', 'D'),
+            ('B', 'B', 'A', 'B'),
+            ('A', 'A', 'C', 'C'),
+        ]);
 
         Ok(Box::new(2))
     }
