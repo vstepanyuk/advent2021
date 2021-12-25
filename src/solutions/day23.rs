f4b8513ab7d867efa50ed4cfce87a8635e7fd7fa
Vitali Stsepaniuk <contact@vitaliy.dev>
2021-12-25 14:40:21 +0100

    refactor: day23

--- src/solutions/day23.rs
+++ src/solutions/day23.rs
@@ -1,5 +1,8 @@
+use std::cmp::{max, min};
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
+use std::thread::sleep;
+use std::time::Duration;
 
 use lazy_static::lazy_static;
 use pathfinding::prelude::dijkstra;
@@ -20,14 +23,15 @@ lazy_static! {
     };
 }
 
-type Room = (char, char);
-type Room2 = (char, char, char, char);
+type Amphipod = char;
+type Hallway = [Amphipod; 11];
+type Room = Vec<Amphipod>;
+type Rooms = [Room; 4];
+type State = (Rooms, Hallway);
 
-#[derive(Eq, PartialEq, Debug, Hash, Copy, Clone)]
-struct State {
-    rooms: [Room; 4],
-    hallway: [char; 11],
-}
+const AMPHIPOD_HALLWAY_MOVES: [usize; 7] = [0, 1, 3, 5, 7, 9, 10];
+
+type Room2 = (char, char, char, char);
 
 #[derive(Eq, PartialEq, Debug, Hash, Copy, Clone)]
 struct State2 {
@@ -35,26 +39,26 @@ struct State2 {
     hallway: [char; 11],
 }
 
-impl Display for State {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "#############")?;
-        let hallway = self.hallway.iter().collect::<String>();
-        writeln!(f, "#{}#", hallway)?;
-        writeln!(
-            f,
-            "###{}#{}#{}#{}###",
-            self.rooms[0].0, self.rooms[1].0, self.rooms[2].0, self.rooms[3].0,
-        )?;
-        writeln!(
-            f,
-            "  #{}#{}#{}#{}#",
-            self.rooms[0].1, self.rooms[1].1, self.rooms[2].1, self.rooms[3].1,
-        )?;
-        writeln!(f, "  #########")?;
-
-        Ok(())
-    }
-}
+// impl Display for StateOld {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+//         writeln!(f, "╭───────────╮")?;
+//         let hallway = self.hallway.iter().collect::<String>();
+//         writeln!(f, "│{}│", hallway)?;
+//         writeln!(f, "╰─╮ ┬ ┬ ┬ ╭─╯")?;
+//         writeln!(
+//             f,
+//             "  │{}│{}│{}│{}│",
+//             self.rooms[0].0, self.rooms[1].0, self.rooms[2].0, self.rooms[3].0,
+//         )?;
+//         writeln!(
+//             f,
+//             "  │{}│{}│{}│{}│",
+//             self.rooms[0].1, self.rooms[1].1, self.rooms[2].1, self.rooms[3].1,
+//         )?;
+//         writeln!(f, "  ╰─┴─┴─┴─╯")?;
+//         Ok(())
+//     }
+// }
 
 impl Display for State2 {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -88,158 +92,99 @@ impl Display for State2 {
 }
 
 impl DaySolution {
-    fn solve1(&self, start_state: [Room; 4]) {
-        let winning_room = [('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D')];
-
-        let state = State {
-            rooms: start_state,
-            hallway: ['.'; 11], // Empty
-        };
-
+    fn solve(&self, initial_rooms: Rooms, winning_rooms: Rooms) -> Option<(Vec<State>, usize)> {
         let result = dijkstra(
-            &state,
+            &(initial_rooms, ['.'; 11]),
             |state| {
-                // println!("Current state\n{}", state);
-                let mut new_states: Vec<(State, usize)> = vec![];
+                println!("Current state");
+                self.draw(state);
 
-                assert_eq!(state.hallway[2], '.');
-                assert_eq!(state.hallway[4], '.');
-                assert_eq!(state.hallway[6], '.');
-                assert_eq!(state.hallway[8], '.');
+                let (rooms, hallway) = state;
+                let mut states: Vec<(State, usize)> = vec![];
 
-                for (room_index, room) in state.rooms.iter().enumerate() {
-                    if winning_room[room_index] == *room {
-                        // println!("Winning room\n{}", state);
-
+                // From rooms
+                for (index, room) in rooms.iter().enumerate() {
+                    if room == &winning_rooms[index] {
                         continue;
                     }
 
-                    let (am1, am2) = room;
-
-                    if *am1 == '.' && *am2 == '.' {
-                        // println!("Empty room\n{}", state);
+                    // Room is empty
+                    if room.iter().all(|a| *a == '.') {
                         continue;
                     }
 
-                    if *am1 == '.' {
-                        let mut new_state = state.clone();
-                        new_state.rooms[room_index] = (*am2, '.');
+                    let room_index = (index + 1) * 2;
+                    for move_to in AMPHIPOD_HALLWAY_MOVES {
+                        let range = min(move_to, room_index)..=max(move_to, room_index);
 
-                        let cost = *COSTS.get(am2).unwrap();
-                        new_states.push((new_state, cost));
-
-                        continue;
-                    }
-
-                    for move_to in [0, 1, 3, 5, 7, 9, 10] {
-                        let room_hallway_index = (room_index + 1) * 2;
-                        let can_move = if move_to < room_hallway_index {
-                            (move_to..room_hallway_index).all(|index| state.hallway[index] == '.')
-                        } else {
-                            (room_hallway_index..=move_to).all(|index| state.hallway[index] == '.')
-                        };
-
-                        if !can_move {
+                        if hallway[range].iter().any(|a| *a != '.') {
+                            // Blocked
                             continue;
                         }
 
-                        let mut new_state = state.clone();
+                        let (mut rooms, mut hallway) = state.clone();
+                        for (distance, amphipod) in rooms[index].iter_mut().enumerate() {
+                            if *amphipod != '.' {
+                                let cost = *COSTS.get(amphipod).unwrap()
+                                    * (distance + self.distance(index, move_to));
 
-                        new_state.hallway[move_to] = *am1;
-                        new_state.rooms[room_index].0 = '.';
+                                hallway[move_to] = *amphipod;
+                                *amphipod = '.';
 
-                        let distance = self.distance(room_index, move_to);
-                        let cost = *COSTS.get(am1).unwrap();
-
-                        new_states.push((new_state, cost * distance));
-                    }
-                }
-
-                let is_debug = false;
-
-                if is_debug {
-                    println!("{}", state);
-                }
-
-                // Moving from hallway
-                for move_from in [0, 1, 3, 5, 7, 9, 10] {
-                    let am = state.hallway[move_from];
-                    if am == '.' {
-                        continue;
-                    }
-
-                    let room_index = match am {
-                        'A' => 0,
-                        'B' => 1,
-                        'C' => 2,
-                        'D' => 3,
-                        _ => unreachable!(),
-                    };
-
-                    let room_hallway_index = (room_index + 1) * 2;
-                    let can_move = if move_from < room_hallway_index {
-                        (move_from + 1..=room_hallway_index)
-                            .all(|index| state.hallway[index] == '.')
-                    } else {
-                        (room_hallway_index..move_from).all(|index| state.hallway[index] == '.')
-                    };
-
-                    if is_debug {
-                        println!("DEBUG {} {}", can_move, am);
-                        println!("{} {}", room_hallway_index, move_from);
-                    }
-
-                    if !can_move {
-                        continue;
-                    }
-
-                    let (am1, am2) = state.rooms[room_index];
-                    if am1 != '.' {
-                        if am2 == '.' {
-                            let mut new_state = state.clone();
-                            new_state.rooms[room_index] = ('.', am1);
-
-                            let cost = *COSTS.get(&am1).unwrap();
-                            new_states.push((new_state, cost));
+                                states.push(((rooms, hallway), cost));
+                                break;
+                            }
                         }
+                    }
+                }
 
+                // To rooms
+                for move_from in AMPHIPOD_HALLWAY_MOVES {
+                    let amphipod = hallway[move_from];
+                    let index = (amphipod as u8 - b'A') as usize;
+
+                    if amphipod == '.' {
                         continue;
                     }
 
-                    let mut new_state = state.clone();
-                    new_state.rooms[room_index] = (am, am2);
-                    new_state.hallway[move_from] = '.';
+                    if rooms[index][0] != '.' {
+                        // Room is full
+                        continue;
+                    }
 
-                    let distance = self.distance(room_index, move_from);
-                    let cost = *COSTS.get(&am).unwrap();
-                    new_states.push((new_state, cost * distance));
+                    let room_index = (index + 1) * 2;
+
+                    let range = min(room_index, move_from) + 1..=max(room_index, move_from) - 1;
+                    if hallway[range].iter().any(|c| *c != '.') {
+                        // Blocked
+                        continue;
+                    }
+
+                    let (mut rooms, mut hallway) = state.clone();
+                    hallway[move_from] = '.';
+
+                    let distance = rooms[index]
+                        .iter()
+                        .position(|a| *a != '.')
+                        .unwrap_or(rooms[index].len())
+                        - 1;
+                    let cost = *COSTS.get(&amphipod).unwrap()
+                        * (distance + self.distance(index, move_from));
+
+                    rooms[index][distance] = amphipod;
+
+                    states.push(((rooms, hallway), cost));
                 }
 
-                if is_debug {
-                    println!("{:?}", new_states);
-                }
-
-                new_states
+                states
             },
-            |state| state.rooms == winning_room,
+            |(rooms, _)| rooms == &winning_rooms,
         );
 
-        let (path, cost) = result.unwrap();
-
-        for p in path.iter() {
-            println!("{}", p);
-        }
-
-        println!("{}", cost);
+        result
     }
 
     fn solve2(&self, start_state: [Room2; 4]) {
-        let mut costs = HashMap::new();
-        costs.insert('A', 1usize);
-        costs.insert('B', 10usize);
-        costs.insert('C', 100usize);
-        costs.insert('D', 1000usize);
-
         let winning_room = [
             ('A', 'A', 'A', 'A'),
             ('B', 'B', 'B', 'B'),
@@ -255,7 +200,7 @@ impl DaySolution {
         let result = dijkstra(
             &state,
             |state| {
-                // println!("Current state\n{}", state);
+                println!("Current state\n{}", state);
                 let mut new_states: Vec<(State2, usize)> = vec![];
 
                 assert_eq!(state.hallway[2], '.');
@@ -436,54 +381,89 @@ impl DaySolution {
     }
 
     fn distance(&self, room_index: usize, position: usize) -> usize {
-        match (room_index, position) {
-            (0, 0) => 3,
-            (0, 1) => 2,
-            (0, 3) => 2,
-            (0, 5) => 4,
-            (0, 7) => 6,
-            (0, 9) => 8,
-            (0, 10) => 9,
-            (1, 0) => 5,
-            (1, 1) => 4,
-            (1, 3) => 2,
-            (1, 5) => 2,
-            (1, 7) => 4,
-            (1, 9) => 6,
-            (1, 10) => 7,
-            (2, 0) => 7,
-            (2, 1) => 6,
-            (2, 3) => 4,
-            (2, 5) => 2,
-            (2, 7) => 2,
-            (2, 9) => 4,
-            (2, 10) => 5,
-            (3, 0) => 9,
-            (3, 1) => 8,
-            (3, 3) => 6,
-            (3, 5) => 4,
-            (3, 7) => 2,
-            (3, 9) => 2,
-            (3, 10) => 3,
-            _ => unreachable!(),
+        return (position as i32 - (room_index as i32 + 1) * 2).abs() as usize + 1;
+    }
+
+    fn parse(&self, input: Option<String>) -> Rooms {
+        let input = input.unwrap();
+        let lines = input
+            .lines()
+            .map(|s| s.chars().collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        let size = lines.len() - 3;
+
+        let mut rooms = [
+            vec!['.'; size],
+            vec!['.'; size],
+            vec!['.'; size],
+            vec!['.'; size],
+        ];
+
+        for i in 0..size {
+            for room in 0..4 {
+                rooms[room][i] = lines[i + 2][(room + 1) * 2 + 1];
+            }
         }
+
+        rooms
+    }
+
+    fn draw(&self, state: &State) {
+        let (rooms, hallway) = state;
+        let mut output = vec![];
+        output.push("╭───────────╮".to_string());
+        output.push(format!("│{}│", hallway.iter().collect::<String>()));
+        output.push("╰─╮ ┬ ┬ ┬ ╭─╯".to_string());
+        for level in 0..rooms[0].len() {
+            output.push(format!(
+                "  │{}│{}│{}│{}│",
+                rooms[0][level], rooms[1][level], rooms[2][level], rooms[3][level]
+            ));
+        }
+        output.push("  ╰─┴─┴─┴─╯".to_string());
+        println!("{}", output.join("\n"));
     }
 }
 
 impl Solution for DaySolution {
-    fn part_1(&mut self, _input: Option<String>) -> Result<Box<dyn Display>> {
-        self.solve1([('A', 'D'), ('C', 'D'), ('B', 'B'), ('A', 'C')]);
+    fn part_1(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
+        let initial = self.parse(input);
+        let winning = [vec!['A'; 2], vec!['B'; 2], vec!['C'; 2], vec!['D'; 2]];
 
-        Ok(Box::new(1))
+        if let Some((path, cost)) = self.solve(initial, winning) {
+            println!();
+            for state in path {
+                print!("\x1B[2J");
+                self.draw(&state);
+                sleep(Duration::from_millis(500));
+            }
+            println!();
+            return Ok(Box::new(cost));
+        }
+
+        Ok(Box::new("No solution"))
     }
 
-    fn part_2(&mut self, _input: Option<String>) -> Result<Box<dyn Display>> {
-        self.solve2([
-            ('A', 'D', 'D', 'D'),
-            ('C', 'C', 'B', 'D'),
-            ('B', 'B', 'A', 'B'),
-            ('A', 'A', 'C', 'C'),
-        ]);
+    fn part_2(&mut self, input: Option<String>) -> Result<Box<dyn Display>> {
+        let initial = self.parse(input);
+        println!("{:?}", initial);
+
+        let winning = [
+            vec!['A', 'A'],
+            vec!['B', 'B'],
+            vec!['C', 'C'],
+            vec!['D', 'D'],
+        ];
+
+        // self.solve2([
+        //     ('A', 'D', 'D', 'D'),
+        //     ('C', 'C', 'B', 'D'),
+        //     ('B', 'B', 'A', 'B'),
+        //     ('A', 'A', 'C', 'C'),
+        // ]);
+
+        let result = self.solve(initial, winning);
+        println!("{:?}", result);
 
         Ok(Box::new(2))
     }
